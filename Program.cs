6d9d14553ad14de35fc14e344028e7052e40639c
FrederikTheisen <FrederikTheisen@gmail.com>
2019-06-04 13:38:17 +0200

    Initial check-in of module IUPred2aPlotterApp

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IUPred2aPlotterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
