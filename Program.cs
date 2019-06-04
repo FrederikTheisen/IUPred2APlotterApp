f15a4e2d3097a704c7ff57eb57c98e01d2dbc0da
Frederik Theisen <frederiktheisen@gmail.com>
2019-06-04 13:45:45 +0200

    ReadDataFile

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 
 namespace IUPred2aPlotterApp
 {
@@ -6,7 +8,33 @@ namespace IUPred2aPlotterApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReadDataFile();
+        }
+
+        static void ReadDataFile()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var dataFile = "IUPred2aPlotterApp.IUPredData.data.txt";
+
+            using (Stream stream = assembly.GetManifestResourceStream(dataFile))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] dat = new string[6];
+                    int i = 0;
+
+                    foreach (var d in line.Split(' '))
+                    {
+                        dat[i] = d.Trim();
+
+                        i++;
+                    }
+                }
+            }
         }
     }
 }
